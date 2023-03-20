c630e9385886023f2461d84ee0497606ab6b99ef
chanr1atwit <chanr1@wit.edu>
2023-03-20 18:09:42 -0400

    new class
    New class for adaptive difficulty algos

---
+++ Adaptive Path/Assets/Scripts/AdaptDiff.cs
@@ -0,0 +1,10 @@
+//new class for adaptive difficulty functions
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdaptDiff : MonoBehavior{
+
+    
+
+}
